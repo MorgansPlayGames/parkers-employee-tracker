97cc84fc2358c1bad773411e5a63380d3a812952
Parker Morgan <jpmaster13@msn.com>
2020-12-05 16:35:24 -0700

    started over the code cause it was poorly written.

--- server.js
+++ server.js
@@ -5,79 +5,56 @@ const q = require('./lib/questions.js');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3001,
+    port: 3306,
     user:"root",
     password:"password",
     database:"employeeDB"
-})
+});
 
 connection.connect(function(err) {
     if (err) throw err;
-})
+    startQ();
+});
 
-function startQ(ask) {
-    inquirer
-    .prompt(ask)
-    .then(answers => {
-        console.log(answers);
-        if(answers.homeBase){
-            theSwitch(answers.homeBase);
-        }
-    })
-    .catch(err => {
-        if(err) throw err;
-    });
+function startQ() {
+    // inquirer
+    // .prompt(q)
+    // .then(answer => {
+    //     console.log(answer)
+    //     switch(answer.homeBase){
+    //         case 'view all employees' : getEmployees(); break;
+    //         case 'view all employees by department' : getEmployees('department'); break;
+    //         case 'view all employees by manager' : getEmployees('manager'); break;
+    //         case 'add employee' : addEmployee(); break;
+    //         case 'add department' : addDepartment(); break;
+    //         case 'add role' : addRole(); break;
+    //         case 'remove employee' : removeEmployee(); break;
+    //         case 'remove role' : removeRole(); break;
+    //         case 'remove department' : removeDepartment(); break;
+    //         case 'update employee role' : updateRole(); break;
+    //         case 'update employee manager' : updateManager(); break;
+    //         case 'view all roles' : viewRoles(); break;
+    //         case 'view all departments' : viewDepartments(); break;
+    //         case 'view budget' : viewBudget(); break;
+    //         case 'quit' : break;
+    //     }
+    // })
+    // .catch(err => {
+    //     if(err) throw err;
+    // });
 }
 
-function theSwitch(searchKey) {
-    switch(searchKey){
-        case 'homeBase' : startQ(q);
-            break;
-        case 'view all employees' : getEmployees();
-            break;
-        case 'view all employees by department' : getEmployees('department');
-            break;
-        case 'view all employees by manager' : getEmployees('manager')
-            break;
-        case 'add employee' :
-            break;
-        case 'add department' :
-            break;
-        case 'add role' :
-            break;
-        case 'remove employee' :
-            break;
-        case 'remove role' :
-            break;
-        case 'remove department' :
-            break;
-        case 'update employee role' :
-            break;
-        case 'update employee manager' :
-            break;
-        case 'view all roles' :
-            break;
-        case 'view all departments' :
-            break;
-        case 'view budget' :
-            break;
-        case 'quit' :
-            break;
-    }
-}
 
-function getEmployees(byType){
-    console.log(byType);
-    switch(byType){
-        case 'department' :
-            console.log('employee by department');
-            break;
-        case 'manager' : 
-            console.log('employee by manager')
-            break;
-    }
 
-    theSwitch('homeBase');
-}
+// function getEmployees(byType){
+//     console.log("by Type " +byType); 
+//     let query ="SELECT * FROM employee_table;";
+//     console.log("here")
+//     console.log(query);
+//     connection.query(query, function(err, res){
+//         if (err) throw err;
+//         console.table(res);
+//         startQ()
+//     });
+// }
 
-theSwitch('homeBase');
