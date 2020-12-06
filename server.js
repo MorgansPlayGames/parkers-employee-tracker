eef7c2edea086a6d350cf3b3651c8b0c58d94598
Parker Morgan <jpmaster13@msn.com>
2020-12-06 00:38:17 -0700

    added queries to their own library for shorthand cleaner code.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const q = require('./lib/questions.js');
+const queryList = require('./lib/query.js');
 
 
 var connection = mysql.createConnection({
@@ -49,13 +50,11 @@ function startQ() {
 
 function getEmployees(byType){
     console.log("by Type " +byType); 
-    query ="SELECT first_name, last_name, title, salary, " +
-    "name as department_name FROM employee LEFT JOIN role ON role_id = role.id " +
-    "LEFT JOIN department ON department_id = department.id";
+    query = queryList.getEmployees
+    
     if(byType === "department") {query += " ORDER BY department_name";}
     if(byType === "manager") {
-        query ="SELECT t.first_name, t.last_name, e.first_name as manager_name, e.last_name as manager_surname" +
-        " FROM employee as t LEFT JOIN employee as e ON t.manager_id = e.id"
+        query = queryList.employeesByManager
     }
     connection.query(query, function(err, res){
         if (err) throw err;
@@ -65,16 +64,14 @@ function getEmployees(byType){
 }
 
 function addEmployee(){
-    const query = "SELECT name, id FROM department"
-    const query2 = "SELECT title, id FROM role"
     let choices = q.addEmployee
-    connection.query(query, function(err, res){
+    connection.query(queryList.deptList, function(err, res){
         if (err) throw err;
         let deptList = res.map(dept => dept.name);
         let queryAdd = new q.queryAdd("department", "Which department is this employee in?", deptList)
         choices.push(queryAdd);
     });
-    connection.query(query2, function(err, res){
+    connection.query(queryList.roleList, function(err, res){
         if (err) throw err;
         let roleList = res.map(role => role.title);
         let queryAdd = new q.queryAdd('role', 'What is this employees title?', roleList);
